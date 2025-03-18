e2d59b398d3c36fb46f5715868484c19d8b5d55a
Gopinath Maddula <maddula.gopinath173@gmail.com>
2025-03-18 21:26:17 +0530

    added creativity

--- src/pages/CaseStudies/CaseStudies.tsx
+++ src/pages/CaseStudies/CaseStudies.tsx
@@ -3,12 +3,31 @@ import './CaseStudies.css';
 import arrow from '../../assets/caseStudiesModal/white-arrow.svg';
 import CsChip from '../../components/CsChip/CsChip';
 import { csChipData } from '../../constants/csConstants';
-import FeaturedProjectsSection from '../../sections/FeaturedProjectsSection/FeaturedProjectsSection';
 import CaseStudyCard from '../../components/CaseStudyCard/CaseStudyCard';
+import circle from '../../assets/circle.svg';
 
 const CaseStudies: React.FC = () => {
   return (
     <div className='case-studies-container'>
+      <div className='case-studies-header'>
+        <span className='case-studies-header-creativity'>creativity</span>
+        <div className='case-studies-header-divider'>
+          <span className='with'>WITH</span>
+          <img
+            src={circle}
+            alt='circle'
+            style={{
+              marginTop: '-75px',
+              marginLeft: '-18px',
+              marginRight: '-135px',
+              width: '454px',
+            }}
+          />
+        </div>
+        <div className='case-studies-header-purpose'>
+          <span>purpose</span>
+        </div>
+      </div>
       <div className='case-studies-intro'>
         <span className='case-studies-intro-text'>
           Looking for my portfolio? You’ll find here some shots of my most
