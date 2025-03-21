5d31e20bf792cc37c68488572fe03b160c035878
Praveen manchi <49606401+praveenmanchi@users.noreply.github.com>
2025-03-21 13:12:24 +0530

    SEO and favicon
    Refactor index.html and update package configurations
    
    - Changed doctype declaration to <!DOCTYPE html> for HTML5 compliance.
    - Enhanced SEO meta tags in index.html for better visibility.
    - Updated favicon and Apple touch icon links for improved branding.
    - Added new dependencies: vite-plugin-html and vite-plugin-sitemap for better build and sitemap generation.
    - Simplified build script in package.json to use vite directly.
    - Refactored Footer component to use a more dynamic approach for social media links.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
+import sitemap from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: "Praveen Manchi - UX/UI Designer",
+          description:
+            "Explore my portfolio showcasing innovative UX/UI design solutions in AI, IoT, and emerging technologies.",
+          keywords:
+            "UX Design, UI Design, Product Design, Web Development, AI, IoT, UX Engineer, Portfolio",
+          favicon: "/favicon.ico",
+          ogImage: "https://praveen-manchi.com/og-image.jpg",
+          ogUrl: "https://praveen-manchi.com",
+        },
+      },
+    }),
+    sitemap({
+      hostname: "https://praveen-manchi.com",
+    }),
+  ],
+});
