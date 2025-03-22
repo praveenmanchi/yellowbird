438ad5107470ba8e40301c25cd29473019dc6b0e
Gopinath Maddula <maddula.gopinath173@gmail.com>
2025-03-22 12:59:54 +0530

    home page changes

---
+++ src/constants/designerIntr0Constants.ts
@@ -0,0 +1,49 @@
+export const tabs = [
+  {
+    id: 'For anyone',
+    label: 'For anyone',
+    beforeHighlighter: "I'm",
+    highlighter: 'Praveen Manchi',
+    description:
+      'designer who cares about making beautiful things that help people.',
+  },
+  {
+    id: 'Recruiters',
+    label: 'Recruiters',
+    beforeHighlighter: "I'm a",
+    highlighter: 'Product designer',
+    description:
+      'with 15 years of experience across brand and product, at companies',
+  },
+  {
+    id: 'Design Directors',
+    label: 'Design Directors',
+    beforeHighlighter:
+      'I take pride in my craft, and love mentoring earlier and thrive in ',
+    highlighter: 'complex, ambiguous environments',
+  },
+  {
+    id: 'Product Designers',
+    label: 'Product Designers',
+    beforeHighlighter: "I'm a",
+    highlighter: 'systems thinker',
+    description:
+      "with a high bar for quality. From process to pixels, I'll collaborate with you.",
+  },
+  {
+    id: 'Product Managers',
+    label: 'Product Managers',
+    beforeHighlighter: 'I bring end-to-end product acumen, from',
+    highlighter: 'vision and strategy',
+    description: 'to discovery and delivery',
+  },
+  {
+    id: 'Engineers',
+    label: 'Engineers',
+    beforeHighlighter: "I'm",
+    highlighter: '{highly_technical}',
+    middler: 'and while',
+    endHighlighter: "(I'm ≠ engineer)",
+    description: 'I know my way / around & can speak "fluently".',
+  },
+];
